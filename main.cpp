e9779ae8f6cee8662b2d9202dad34153a7ad170f
davydavman17 <davydavman70@gmail.com>
2026-05-12 18:55:34 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+   long keyNumber=795763502;
+   int phoneNumbers[3];
+   int i=0;
+ //filling numbers
+    do{
+
+    cout << "Enter your phone number" << endl;
+    cin>>phoneNumbers[i];
+    i=i+1;
+    }
+        while(i<3);
+        //linear search
+        //linear Davy
+        for(int i=0; i<3;i++){
+
+            if(phoneNumbers[i]==keyNumber) {
+            cout<<"Davy number's found"<<endl;
+        break;
+}
+}
+return 0;
+}
